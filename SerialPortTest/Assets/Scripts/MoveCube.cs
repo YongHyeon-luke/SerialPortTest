5de95e4274e151a4a6362e9f7ae72ff3c75d6755
YongHyeon_Luke <horangwhale@gmail.com>
2018-06-01 14:48:22 +0900

    큐브돌리기용 스크립트

---
+++ SerialPortTest/Assets/Scripts/MoveCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour {
+
+    public Transform myTransfrom;
+
+	// Use this for initialization
+	void Start () {
+        myTransfrom = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myTransfrom.Rotate(new Vector3(DeviceData.Pitch, DeviceData.Yaw, DeviceData.Roll));
+	}
+}

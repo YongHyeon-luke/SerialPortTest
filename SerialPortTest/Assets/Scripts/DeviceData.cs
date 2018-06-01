14e0ae6b7ffe7ece2db8750bb3ad0a47558a7a1a
YongHyeon_Luke <horangwhale@gmail.com>
2018-06-01 14:48:09 +0900

    Device상태를 저장하는 스크립트

---
+++ SerialPortTest/Assets/Scripts/DeviceData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public static class DeviceData {
+
+    public static int SpeedCount { get; set; }
+    public static int RotaryCount { get; set; }
+    public static int Handle { get; set; }
+   // public static int Button { get; set; }
+
+    public static bool BTN_L1 { get; set; }
+    public static bool BTN_L2 { get; set; }
+    public static bool BTN_R1 { get; set; }
+    public static bool BTN_R2 { get; set; }
+
+    public static bool Limit_Left { get; set; }
+    public static bool Limit_Center { get; set; }
+    public static bool Limit_Right { get; set; }
+
+    public static float Roll { get; set; }
+    public static float Pitch { get; set; }
+    public static float Yaw { get; set; }
+}
